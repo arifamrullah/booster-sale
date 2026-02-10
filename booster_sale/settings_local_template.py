d97e9ba539db717487c9971bf55ad10a8e0e44e3
Arif Amrullah <arifamrullah17@gmail.com>
2026-02-10 16:34:43 +0700

    local settings template

---
+++ booster_sale/settings_local_template.py
@@ -0,0 +1,14 @@
+from . settings import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
